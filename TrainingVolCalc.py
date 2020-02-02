7a5e90bc27652bd94b94dc5c89d6e78618aaf58c
Tomi Rämö <47462643+TomiRam@users.noreply.github.com>
2020-02-02 13:07:50 +0200

    Create TrainingVolCalc.py

---
+++ TrainingVolCalc.py
@@ -0,0 +1,5 @@
+#This is the training volume calculator done in pyhton
+weight = 130; #enter total weight lifted
+reps = 3; #enter total reps done
+sets = 3; #enter total sets done
+print(weight*reps*sets);
